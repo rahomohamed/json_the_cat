9e4733741a5282a285e8a9c6c25ee0b350574fd3
rahomohamed <rahojmohamed@gmail.com>
2020-02-13 22:22:04 +0000

    add files

--- test/breedFetcherTest.js
+++ test/breedFetcherTest.js
@@ -9,7 +9,7 @@ describe('fetchBreedDescription', () => {
       // we expect no error for this scenario
       assert.equal(err, null);
 
-      const expectedDesc = "The Siberians dog like temperament and affection makes the ideal lap cat and will live quite happily indoors. Very agile and powerful, the Siberian cat can easily leap and reach high places, including the tops of refrigerators and even doors.";
+      const expectedDesc = "The Siberians dog like temperament and affection makes the ideal lap cat and will live quite happily indoors. Very agile and powerful, the Siberian cat can easily leap and reach high places, including the tops of refrigerators and even doors. ";
 
       // compare returned description
       assert.equal(expectedDesc, desc);
