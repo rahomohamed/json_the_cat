28bd33fb33d224596b7ef45b3f242f2f497085dc
rahomohamed <rahojmohamed@gmail.com>
2020-02-13 22:20:31 +0000

    add files

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,29 @@
 const request = require("request");
-let breed = process.argv[2];
-let api = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`
 
-request(api, (error, response, body) => {
-  if (error) throw error;
-  console.log('no data found');
-  console.log("error:", error); // Print the error if one occurred
-  console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
-  console.log("body:", body); // Print the HTML for the Google homepage.
-  const data = JSON.parse(body);
-  console.log(data);
-  console.log(data[0].description);
-  console.log(typeof data);
+const fetchBreedDescription = function(breed, callback) {
+  let api = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
+
+  request(api, (error, response, body) => {
+    if (error) {
+      callback(error, null); // if there is an error callback error
+      return;
+    }
+    const data = JSON.parse(body); // if no error parse data (change to obj) & return null for
+    if (data[0]) {
+      callback(null, data[0].description);
+    
+    } else {
+      callback(null, 'no breed exists');
+    }
   })
+}
 
+module.exports = { fetchBreedDescription };
 
+// fetchBreedDescription("Russian", (err, data) => {
+//   if (err) {
+//     console.log("error function");
+//   } else {
+//     console.log("got info", data);
+//   }
+// });
---
+++ index.js
@@ -0,0 +1,12 @@
+const { fetchBreedDescription } = require('./breedFetcher');
+
+const breedName = process.argv[2];
+const doSomething = (error, desc) => {
+  if (error) {
+    console.log('Error fetch details:', error);
+  } else {
+    console.log(desc);
+  }
+}
+
+fetchBreedDescription(breedName, doSomething);
